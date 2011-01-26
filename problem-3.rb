9bef3aed2529f7d5bab3f82b8bb7bd91ab239510
Ram <ram.nadella@live.com>
2011-01-25 23:44:19 -0800

    Modified functions in problems 3 and 4 to follow Ruby conventions, function name changed to prime? instead of is_prime

--- problem-3.rb
+++ problem-3.rb
@@ -3,7 +3,7 @@
 # What is the largest prime factor of the number 60085147514
 
 
-def is_prime?(number)
+def prime?(number)
   (2..number-1).each do |n|
     if number%n == 0
       return false
@@ -12,7 +12,7 @@ def is_prime?(number)
   return true
 end
 
-def is_factor?(current_number, number)
+def factor?(current_number, number)
   if number%current_number == 0
     return true
   else
@@ -22,7 +22,7 @@ end
 
 def get_first_prime_factor(number)
   (2..number-1).each do |current_number|
-    if is_factor?(current_number, number) && is_prime?(current_number)
+    if factor?(current_number, number) && prime?(current_number)
       return current_number
     end
   end
@@ -31,7 +31,7 @@ end
 number = 600851475143
 prime_factors = []
 n = number
-until is_prime?(n)
+until prime?(n)
   prime_factor = get_first_prime_factor(n)
   prime_factors << prime_factor
   n = n / prime_factor
--- problem-5.rb
+++ problem-5.rb
@@ -7,7 +7,7 @@
 
 # There is a chance to cut some run time by changing the order of the divisors.
 
-def is_divisible_by_1to20?(number)
+def divisible_by_1to20?(number)
   divisors = [19, 18, 17, 16, 15, 14, 13, 12, 11]
   divisors.each do |n|
     if (number%n != 0)
@@ -18,7 +18,7 @@ def is_divisible_by_1to20?(number)
 end
 
 number, base = 20, 1
-until is_divisible_by_1to20?(number)
+until divisible_by_1to20?(number)
   number = base*20
   base += 1
 end

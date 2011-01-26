8c34ccc69b2d618949865e13bc4db77cf8aa2fda
Ram <ram.nadella@live.com>
2011-01-25 22:50:14 -0800

    Solved problem 5. Achieved significant run time improvement by eliminating the unnecessary divisors. Divisible by 1 to 20 problem.

---
+++ problem-5.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+
+# The divisors are limited because there is no need to check disivion by all the numbers
+# from 1 to 20. For eg. if number is divisible by 18, we know it's all divisible by 2, 3, 6 and 9
+
+# There is a chance to cut some run time by changing the order of the divisors.
+
+def is_divisible_by_1to20?(number)
+  divisors = [19, 18, 17, 16, 15, 14, 13, 12, 11]
+  divisors.each do |n|
+    if (number%n != 0)
+      return false
+    end
+  end
+  return true
+end
+
+number, base = 20, 1
+until is_divisible_by_1to20?(number)
+  number = base*20
+  base += 1
+end
+
+puts "Answer: #{number}"

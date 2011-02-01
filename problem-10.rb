6245fea6bde66543e4ab70fa141d1499a74c3874
Ram <ram.nadella@live.com>
2011-01-31 20:57:12 -0800

    Solved problem 10

---
+++ problem-10.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+# Find the sum of all the primes below two million.
+
+def prime?(number)
+  if number == 1
+    return false
+  elsif number < 4
+    return true
+  elsif number%2 == 0
+    return false
+  elsif number < 9
+    return true
+  elsif number%3 == 0
+    return false
+  else
+    r = (Math.sqrt(number)).floor
+    f = 5
+    while f<=r
+      if number%f == 0
+        return false
+      elsif number%(f+2) == 0
+        return false
+      end
+      f += 6
+    end
+    return true
+  end
+end
+
+sum_of_primes = 0
+(2..2000000).each do |n|
+  if prime?(n)
+    sum_of_primes += n
+  end
+end
+
+puts "Answer: " + sum_of_primes.to_s

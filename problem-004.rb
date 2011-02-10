51a4b6c669441f34f4c58ca0b5a18d62bb73ccec
Ram <ram.nadella@live.com>
2011-02-09 22:29:40 -0800

    Optimized the code to reduce the run time

--- problem-004.rb
+++ problem-004.rb
@@ -2,7 +2,7 @@
 
 # Find the largest palindrome made from the product of two 3-digit numbers
 
-def is_palindrome?(number)
+def palindrome?(number)
   number_array = number.abs.to_s.split("")
   index = number_array.size-1
   (0..number_array.size/2).each do |index|
@@ -16,9 +16,9 @@ end
 highest_palindrome = 0
 
 (100..999).each do |x|
-  (100..999).each do |y|
+  (x..999).each do |y|
     xy = x*y
-    if is_palindrome?(xy) && xy > highest_palindrome
+    if palindrome?(xy) && xy > highest_palindrome
       highest_palindrome = xy
     end
   end

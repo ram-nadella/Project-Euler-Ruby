9c94b1fa9013188f905ff90bafc1249333f418d8
Ram <ram.nadella@live.com>
2011-02-10 21:12:52 -0800

    Solved problem 42.

---
+++ problem-042.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+# Using words.txt (right click and 'Save Link/Target As...'), a 16K text file 
+# containing nearly two-thousand common English words, how many are 
+# triangle words?
+
+
+def alpha_score(word, alpha_values)
+  score = 0
+  word.split("").each do |alphabet|
+    score += alpha_values[alphabet]
+  end
+  return score
+end
+
+def triangle_number?(number)
+  root1 = (-1+Math.sqrt(1+(8*number)))/2
+  root2 = (-1-Math.sqrt(1+(8*number)))/2
+  root = 0
+  if root1 > root2
+    root = root1
+  else
+    root = root2
+  end
+  if (root-root.to_i) == 0
+    return true
+  else
+    return false
+  end
+end
+
+
+words_file_contents = ""
+File.open('data/words.txt').each do |line|
+  words_file_contents << line
+end
+
+words = words_file_contents.split(",").map do |word|
+  word.sub("\"", "").sub("\"", "")
+end
+
+# generate the alphabetical values hash
+alpha_values = {}
+value = 1
+('A'..'Z').each do |alphabet|
+  alpha_values[alphabet] = value
+  value += 1
+end
+
+word_scores = words.map do |word|
+  alpha_score(word, alpha_values)
+end
+
+triangle_number_count = 0
+
+word_scores.each do |score|
+  if triangle_number?(score)
+    triangle_number_count += 1
+  end
+end
+
+puts "Answer: #{triangle_number_count}"
